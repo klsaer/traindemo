613f11261b0fb9b787ce4c09eb515e583fc4c4e2
Menshenin Vlad <jedismooth.inc@mail.ru>
2024-11-25 13:47:32 +0500

    controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\public\img;
+
+class MainController extends Controller
+{
+    public function showIndex(){
+        return view('home');
+    }
+
+    public function showCards(){
+        $cards = [
+            [
+                'id'=>1, 'title'=>"Продукт 1", 'price' => 500,
+                'path' =>"./public/img/1.jpg",
+            ],
+            [
+                'id'=>2, 'title'=>"Продукт 2", 'price' => 600,
+                'path' =>"./public/img/1.jpg",
+            ]
+            ];
+            return view('home',compact('cards'));
+    }
+}
